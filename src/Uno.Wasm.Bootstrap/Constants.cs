f074587232c7b02b1d8bea2f958ec56a63dbf6f3
Jérôme Laban <jerome.laban@nventive.com>
2018-11-14 11:13:47 -0500

    Bump to latest mono-wasm build

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -6,7 +6,7 @@ namespace Uno.Wasm.Bootstrap
 {
 	internal class Constants
 	{
-		public const string DefaultSdkUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1331/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1331/ubuntu-1804-amd64/sdks/wasm/mono-wasm-a682a3b2b63.zip";
-		public const string DefaultAotUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1331/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1331/ubuntu-1804-amd64/wasm-release-Linux-a682a3b2b635194a22e7e4bb93088e85c922fb95.zip";
+		public const string DefaultSdkUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1450/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1450/ubuntu-1804-amd64/sdks/wasm/mono-wasm-10c7e9f7229.zip";
+		public const string DefaultAotUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1450/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1450/ubuntu-1804-amd64/wasm-release-Linux-10c7e9f722966a9f066962c6407824bcddf0c880.zip";
 	}
 }
