76be743c81845afa64af7e8d53258539bf0c220d
Jérôme Laban <jerome.laban@nventive.com>
2018-12-13 13:02:01 -0500

    Update mono-wasm sdk
    Fixes https://github.com/mono/mono/pull/12033

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -6,7 +6,7 @@ namespace Uno.Wasm.Bootstrap
 {
 	internal class Constants
 	{
-		public const string DefaultSdkUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1534/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1534/ubuntu-1804-amd64/sdks/wasm/mono-wasm-c93db5b0471.zip";
-		public const string DefaultAotSDKUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1534/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1534/ubuntu-1804-amd64/wasm-release-Linux-c93db5b04710cfd505c9672ed4f7263319e77abf.zip";
+		public const string DefaultSdkUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1635/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1635/ubuntu-1804-amd64/sdks/wasm/mono-wasm-01f6a0203b0.zip";
+		public const string DefaultAotSDKUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1635/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1635/ubuntu-1804-amd64/wasm-release-Linux-01f6a0203b0566dc845eefb453e345f7ede77803.zip";
 	}
 }
