cb59a03acd8c9c63547ebab9de88d8a6563c2011
Jérôme Laban <jerome.laban@nventive.com>
2019-03-31 16:44:13 -0400

    Update to mono e894d683f9f0a9c7351b513d3f015a5e170449eb
    Adresses https://github.com/mono/mono/issues/12871

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -6,7 +6,7 @@ namespace Uno.Wasm.Bootstrap
 {
 	internal class Constants
 	{
-		public const string DefaultSdkUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/label=ubuntu-1804-amd64/2418/Azure/processDownloadRequest/2418/ubuntu-1804-amd64/sdks/wasm/mono-wasm-0b58483fa81.zip";
-		public const string DefaultAotSDKUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/label=ubuntu-1804-amd64/2418/Azure/processDownloadRequest/2418/ubuntu-1804-amd64/wasm-release-Linux-0b58483fa814707b8a98833a48153ac389d9d933.zip";
+		public const string DefaultSdkUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/label=ubuntu-1804-amd64/2495/Azure/processDownloadRequest/2495/ubuntu-1804-amd64/sdks/wasm/mono-wasm-e894d683f9f.zip";
+		public const string DefaultAotSDKUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/label=ubuntu-1804-amd64/2495/Azure/processDownloadRequest/2495/ubuntu-1804-amd64/wasm-release-Linux-e894d683f9f0a9c7351b513d3f015a5e170449eb.zip";
 	}
 }
