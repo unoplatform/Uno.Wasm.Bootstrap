3bc856532860837a45f26e98c16b76fa1a387968
Jérôme Laban <jerome.laban@nventive.com>
2019-04-26 08:18:22 -0400

    Update mono master
    Mono commit: d44e45c32ec1315f5ab97b2e2ec5104ccad6e868

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -6,7 +6,7 @@ namespace Uno.Wasm.Bootstrap
 {
 	internal class Constants
 	{
-		public const string DefaultSdkUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/label=ubuntu-1804-amd64/2495/Azure/processDownloadRequest/2495/ubuntu-1804-amd64/sdks/wasm/mono-wasm-e894d683f9f.zip";
-		public const string DefaultAotSDKUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/label=ubuntu-1804-amd64/2495/Azure/processDownloadRequest/2495/ubuntu-1804-amd64/wasm-release-Linux-e894d683f9f0a9c7351b513d3f015a5e170449eb.zip";
+		public const string DefaultSdkUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/label=ubuntu-1804-amd64/2703/Azure/processDownloadRequest/2703/ubuntu-1804-amd64/sdks/wasm/mono-wasm-d44e45c32ec.zip";
+		public const string DefaultAotSDKUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/label=ubuntu-1804-amd64/2703/Azure/processDownloadRequest/2703/ubuntu-1804-amd64/wasm-release-Linux-d44e45c32ec1315f5ab97b2e2ec5104ccad6e868.zip";
 	}
 }
