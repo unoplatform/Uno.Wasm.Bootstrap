4c90c646bcec822d141dddbcbe2674c85c66863c
Jerome Laban <jerome.laban@nventive.com>
2019-11-02 15:44:13 -0400

    Reference commit-specific patch files

--- build/gitpod/build-all.sh
+++ build/gitpod/build-all.sh
@@ -8,8 +8,8 @@ echo Installing emscripten $EMSCRIPTEN_VERSION
 ./emsdk install $EMSCRIPTEN_VERSION
 ./emsdk activate $EMSCRIPTEN_VERSION
 
-wget https://raw.githubusercontent.com/mono/mono/master/sdks/builds/emscripten-pr-8457.diff
-wget https://raw.githubusercontent.com/mono/mono/master/sdks/builds/fix-emscripten-8511.diff
+wget https://raw.githubusercontent.com/mono/mono/27247739c68faee7b2a63ae805222d4b375d2161/sdks/builds/emscripten-pr-8457.diff
+wget https://raw.githubusercontent.com/mono/mono/27247739c68faee7b2a63ae805222d4b375d2161/sdks/builds/fix-emscripten-8511.diff
 
 # apply patches
 cd upstream/emscripten
@@ -17,6 +17,7 @@ patch -N -p1 < ~/emsdk/emscripten-pr-8457.diff
 patch -N -p1 < ~/emsdk/fix-emscripten-8511.diff
 
 cd /workspace/Uno.Wasm.Bootstrap
+
 source ~/emsdk/emsdk_env.sh
 
 msbuild /r /t:Publish /p:Configuration=Release src/Uno.Wasm.Bootstrap.sln
