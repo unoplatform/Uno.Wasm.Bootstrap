147c8501ced1fed22d4c16763cb66d8ee2c0616e
Jérôme Laban <jerome.laban@nventive.com>
2019-12-29 12:54:00 -0500

    Update Microsoft.NETFramework.ReferenceAssemblies to 1.0.0

--- src/Uno.Wasm.Bootstrap/UnoInstallSDKTask.cs
+++ src/Uno.Wasm.Bootstrap/UnoInstallSDKTask.cs
@@ -142,7 +142,7 @@ namespace Uno.Wasm.Bootstrap
 					  </PropertyGroup>
 						<ItemGroup>
 							<!-- Enable cross targeting, in case net462 is not installed on the current machine -->
-							<PackageReference Include=""Microsoft.NETFramework.ReferenceAssemblies"" Version=""1.0.0-preview.2"" PrivateAssets=""All"" />
+							<PackageReference Include=""Microsoft.NETFramework.ReferenceAssemblies"" Version=""1.0.0"" PrivateAssets=""All"" />
 
 							<Reference Include=""Mono.Cecil"">
 								<HintPath>{SdkPath}/Mono.Cecil.dll</HintPath>
