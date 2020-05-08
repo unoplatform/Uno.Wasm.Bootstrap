430b5660cd30d852ff447d49cfd807531f309d06
Jerome Laban <jerome.laban@nventive.com>
2020-05-08 09:06:31 -0400

    chore: Update to latest mono
    Mono base SHA1: 049de7513a894cae5581c35f3ee0b40f9f2d5f1a
    Build https://github.com/unoplatform/Uno.Wasm.MonoRuntime/commit/d43e1064b486c1fb11b4cea72f3dbca836341670

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -7,8 +7,8 @@ namespace Uno.Wasm.Bootstrap
 	internal class Constants
 	{
 		// NOTE: The SDK version may be overriden by an installation of the https://www.nuget.org/packages/Uno.Wasm.MonoRuntime nuget package
-		public const string DefaultSdkUrl = @"https://unowasmbootstrap.blob.core.windows.net/runtime/mono-wasm-0b57d723c10.zip";
-		public const string DefaultAotSDKUrl = @"https://unowasmbootstrap.blob.core.windows.net/runtime/wasm-release-Linux-0b57d723c1092311669dd5f4d36f3ade4b2a66eb.zip";
+		public const string DefaultSdkUrl = @"https://unowasmbootstrap.blob.core.windows.net/runtime/mono-wasm-502dca36d36.zip";
+		public const string DefaultAotSDKUrl = @"https://unowasmbootstrap.blob.core.windows.net/runtime/wasm-release-Linux-502dca36d361ce0ebc9e9c3a2f1d43ff8b953be7.zip";
 
 		/// <summary>
 		/// Min version of the emscripten SDK. Must be aligned with mono's SDK build in <see cref="DefaultAotSDKUrl"/>.
