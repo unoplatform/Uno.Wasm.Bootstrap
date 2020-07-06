f088f3935c2bd1cdfccafaef18ef00ae66143d7b
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2020-07-06 18:31:52 -0400

    feat: Add Uno.Wasm.Static Linking sample

---
+++ src/Uno.Wasm.Static Linking/WasmScripts/test.js
@@ -0,0 +1,23 @@
+﻿define(() => {
+    document.body.innerHTML = "<div id='results' />";
+});
+
+var Interop = {
+    appendResult: function (str) {
+        var txt = document.createTextNode(str);
+        var parent = document.getElementById('results');
+        parent.appendChild(txt, parent.lastChild);
+    }
+};
+
+var Validation = {
+    validateEmAddFunction: function (str) {
+        var fPtr = Module.addFunction((state) => { }, 'vi');
+
+        if (!fPtr) {
+            throw `Could not execute emscripten addFunction`;
+        }
+
+        return fPtr;
+    }
+};
