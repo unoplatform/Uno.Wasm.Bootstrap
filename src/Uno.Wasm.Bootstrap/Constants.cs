9f1f31dc84b911ac878d11986f06826823c7ddaa
Jerome Laban <jerome.laban@nventive.com>
2020-08-13 10:35:36 -0400

    feat: Update to mono latest

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -7,8 +7,8 @@ namespace Uno.Wasm.Bootstrap
 	internal class Constants
 	{
 		// NOTE: The SDK version may be overriden by an installation of the https://www.nuget.org/packages/Uno.Wasm.MonoRuntime nuget package
-		public const string DefaultSdkUrl = @"https://unowasmbootstrap.blob.core.windows.net/runtime/mono-wasm-dd3c370ef1b.zip";
-		public const string DefaultAotSDKUrl = @"https://unowasmbootstrap.blob.core.windows.net/runtime/wasm-release-Linux-dd3c370ef1bd65313abdb4062688011d5e975ef1.zip";
+		public const string DefaultSdkUrl = @"https://unowasmbootstrap.azureedge.net/runtime/mono-wasm-ee6d133e340.zip";
+		public const string DefaultAotSDKUrl = @"https://unowasmbootstrap.azureedge.net/runtime/wasm-release-Linux-ee6d133e3405174b19bc0046d2a85bebd961b1ea.zip";
 
 		/// <summary>
 		/// Min version of the emscripten SDK. Must be aligned with mono's SDK build in <see cref="DefaultAotSDKUrl"/>.
@@ -17,6 +17,6 @@ namespace Uno.Wasm.Bootstrap
 		/// The emscripten version use by mono can be found here:
 		/// https://github.com/mono/mono/blob/master/sdks/builds/wasm.mk#L4
 		/// </remarks>
-		public static Version EmscriptenMinVersion { get; } = new Version("1.39.20");
+		public static Version EmscriptenMinVersion { get; } = new Version("1.40.0");
 	}
 }
