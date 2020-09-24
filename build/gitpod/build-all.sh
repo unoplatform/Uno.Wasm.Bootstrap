faf1cd6f09d5568035d99023beb13992eba63b10
Jerome Laban <jerome.laban@nventive.com>
2020-09-24 14:12:07 -0400

    fix(gitpod): Remove explicit emscripten installation

--- build/gitpod/build-all.sh
+++ build/gitpod/build-all.sh
@@ -2,21 +2,6 @@
 export EMSCRIPTEN_VERSION=1.39.11
 export NUGET_PACKAGES=/workspace/.nuget
 
-cd ~
-git clone --branch $EMSCRIPTEN_VERSION https://github.com/emscripten-core/emsdk.git
-cd emsdk
-echo Installing emscripten $EMSCRIPTEN_VERSION
-./emsdk install $EMSCRIPTEN_VERSION
-./emsdk activate $EMSCRIPTEN_VERSION
-
-wget https://raw.githubusercontent.com/mono/mono/b777471fcace85325e2c2af0e460f4ecd8059b5a/sdks/builds/emscripten-pr-8457.diff
-wget https://raw.githubusercontent.com/mono/mono/b777471fcace85325e2c2af0e460f4ecd8059b5a/sdks/builds/fix-emscripten-8511.diff
-
-# apply patches
-cd upstream/emscripten
-patch -N -p1 < ~/emsdk/emscripten-pr-8457.diff
-patch -N -p1 < ~/emsdk/fix-emscripten-8511.diff
-
 cd /workspace/Uno.Wasm.Bootstrap
 
 source ~/emsdk/emsdk_env.sh
