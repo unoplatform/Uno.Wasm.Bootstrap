0334ca2d587c3c5fc1da329caa2ed73082a101d7
Jerome Laban <jerome.laban@nventive.com>
2021-01-11 11:03:33 -0500

    chore: Update to latest dotnet/runtime

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -20,7 +20,7 @@ namespace Uno.Wasm.Bootstrap
 		public static Version MonoRuntimeEmscriptenVersion { get; } = new Version("2.0.6");
 
 		// NOTE: The SDK version may be overriden by an installation of the https://www.nuget.org/packages/Uno.Wasm.MonoRuntime nuget package
-		public const string DefaultDotnetRuntimeSdkUrl = "https://unowasmbootstrap.azureedge.net/runtime/dotnet-runtime-wasm-acced89-23262-Release.zip";
+		public const string DefaultDotnetRuntimeSdkUrl = "https://unowasmbootstrap.azureedge.net/runtime/dotnet-runtime-wasm-6c530cf-23391-Release.zip";
 
 		/// <summary>
 		/// Min version of the emscripten SDK. Must be aligned with dotnet/runtime SDK build in <see cref="NetCoreWasmSDKUri"/>.
