a433ef111b4135b2d69e377ab537b980dfb8e986
Jerome Laban <jerome.laban@nventive.com>
2021-06-25 15:29:35 -0400

    chore: Update to runtime latest, bump to emscripten 2.0.23

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -6,7 +6,7 @@ namespace Uno.Wasm.Bootstrap
 {
 	internal class Constants
 	{
-		public const string DefaultDotnetRuntimeSdkUrl = "https://unowasmbootstrap.azureedge.net/runtime/dotnet-runtime-wasm-linux-9079e3a-89313ca0693-884011754-Release.zip";
+		public const string DefaultDotnetRuntimeSdkUrl = "https://unowasmbootstrap.blob.core.windows.net/runtime/dotnet-runtime-wasm-linux-d69fec7-881e90231b9-971517687-Release.zip";
 
 		/// <summary>
 		/// Min version of the emscripten SDK. Must be aligned with dotnet/runtime SDK build in <see cref="NetCoreWasmSDKUri"/>.
@@ -15,6 +15,6 @@ namespace Uno.Wasm.Bootstrap
 		/// The emscripten version use by dotnet/runtime can be found here:
 		/// https://github.com/dotnet/runtime/blob/f9bb1673708ca840da0e71f9a9444ea9b0d31911/src/mono/wasm/Makefile#L32
 		/// </remarks>
-		public static Version DotnetRuntimeEmscriptenVersion { get; } = new Version("2.0.12");
+		public static Version DotnetRuntimeEmscriptenVersion { get; } = new Version("2.0.23");
 	}
 }
