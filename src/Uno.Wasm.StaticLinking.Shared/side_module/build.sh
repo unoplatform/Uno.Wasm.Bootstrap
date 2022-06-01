51fe67d8ef23bf21780649f76cac2ada8a7c665c
Jerome Laban <jerome.laban@nventive.com>
2022-06-01 11:53:55 -0400

    ci: rebuild side modules

--- src/Uno.Wasm.StaticLinking.Shared/side_module/build.sh
+++ src/Uno.Wasm.StaticLinking.Shared/side_module/build.sh
@@ -3,7 +3,7 @@ emcc main.cpp -s MAIN_MODULE=1 -o hello.html -s WASM=1 -s EXPORT_ALL=1 --pre-js 
 
 buildSideModule(){
 	mkdir -p `dirname $2`
-	emcc $1 -std=c++17 -s LEGALIZE_JS_FFI=0 -r -o $2 -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=0 -DCUSTOM_VERSION="\"$3\"" -DCUSTOM_FUNCTION_NAME="$4_getCustomVersion"
+	emcc $1 -std=c++17 -s LEGALIZE_JS_FFI=0 -r -o $2 -s WASM=1 -s USE_LIBPNG=1 -s DISABLE_EXCEPTION_CATCHING=0 -DCUSTOM_VERSION="\"$3\"" -DCUSTOM_FUNCTION_NAME="$4_getCustomVersion"
 }
 
 buildSideModule "mysideModule.cpp" "../native/side.bc" "1.2" "side"
@@ -15,5 +15,5 @@ buildSideModule "version_test.cpp" "../native/side3.bc/1.4/side3.bc" "1.4" "side
 
 buildSideModule "version_test.cpp" "../native/side4.bc/1.3/side4.bc" "1.3" "side4"
 buildSideModule "version_test.cpp" "../native/side4.bc/2.0/side4.bc" "2.0" "side4"
-buildSideModule "version_test.cpp" "../native/side4.bc/3.0/side4.bc" "3.0" "side4"
+buildSideModule "version_test.cpp" "../native/side4.bc/3.1/side4.bc" "3.1" "side4"
 buildSideModule "version_test.cpp" "../native/side4.bc/5.0/side4.bc" "5.0" "side4"
