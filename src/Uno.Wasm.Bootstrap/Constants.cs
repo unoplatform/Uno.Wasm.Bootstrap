002cbd8d64a9796db7ca197fa33cb20718fc446d
Jerome Laban <jerome.laban@nventive.com>
2022-06-18 23:00:05 -0400

    chore: Update to fix AOT compiler crash

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -7,7 +7,7 @@ namespace Uno.Wasm.Bootstrap
 	internal class Constants
 	{
 		public const string DefaultDotnetRuntimeSdkUrl = "https://unowasmbootstrap.azureedge.net/runtime/"
-			+ "dotnet-runtime-wasm-linux-33813c4-59b2dd9fa37-2492148814-Release.zip";
+			+ "dotnet-runtime-wasm-linux-fb487ce-59b2dd9fa37-2522463601-Release.zip";
 
 		/// <summary>
 		/// Min version of the emscripten SDK. Must be aligned with dotnet/runtime SDK build in <see cref="NetCoreWasmSDKUri"/>.
