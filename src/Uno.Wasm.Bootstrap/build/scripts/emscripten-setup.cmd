a12854baf924cfa7323020cc2d1bc6e4aaaa747d
Jerome Laban <jerome.laban@nventive.com>
2022-10-11 08:24:48 -0400

    fix: Adjust python validation on windows
    (cherry picked from commit 703e5e23979a400f5c403209ec6d9aa7f741b22f)
    
    # Conflicts:
    #	src/Uno.Wasm.Bootstrap/build/scripts/emscripten-setup.cmd

--- src/Uno.Wasm.Bootstrap/build/scripts/emscripten-setup.cmd
+++ src/Uno.Wasm.Bootstrap/build/scripts/emscripten-setup.cmd
@@ -18,7 +18,15 @@ IF NOT EXIST "%UNO_EMSDK_PATH%" (
 
 pushd "%UNO_EMSDK_PATH%"
 
+python --version
+
+IF ERRORLEVEL 1 ( 
+	echo Python is not installed. You can install it from the store https://www.microsoft.com/store/productId/9P7QFQMJRFP7  1>&2
+	EXIT /B 1
+)
+
 IF NOT EXIST "%UNO_EMSDK_PATH_MARKER%" (
+<<<<<<< HEAD
 	python --version
 
 	IF ERRORLEVEL 1 ( 
@@ -34,6 +42,17 @@ IF NOT EXIST "%UNO_EMSDK_PATH_MARKER%" (
 		echo "Writing %UNO_EMSDK_PATH_MARKER%"
 		echo "installed" > "%UNO_EMSDK_PATH_MARKER%"
 	)
+=======
+	echo "Installing emscripten %UNO_EMSDK_VERSION% in %UNO_EMSDK_PATH%"
+
+	git clone --branch %UNO_EMSDK_VERSION% https://github.com/emscripten-core/emsdk 2>&1
+	cd emsdk
+	emsdk install %UNO_EMSDK_VERSION% 2>&1
+	emsdk activate %UNO_EMSDK_VERSION% 2>&1
+
+	echo "Writing %UNO_EMSDK_PATH_MARKER%"
+	echo "installed" > "%UNO_EMSDK_PATH_MARKER%"
+>>>>>>> 703e5e2 (fix: Adjust python validation on windows)
 ) ELSE (
 	echo "Skipping installed emscripten %UNO_EMSDK_VERSION% in %UNO_EMSDK_PATH%"
 )
