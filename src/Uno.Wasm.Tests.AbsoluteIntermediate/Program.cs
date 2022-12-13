9e0ba031bfa1757ce80ae5607e98a57420d00e10
Jerome Laban <jerome.laban@nventive.com>
2022-12-13 08:24:53 -0500

    chore: Add IntermediateOutputPath change validation

---
+++ src/Uno.Wasm.Tests.AbsoluteIntermediate/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno.Wasm.Test.Empty
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Main");
+		}
+	}
+}
