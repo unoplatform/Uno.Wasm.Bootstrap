845ba7bc5b4f179f98a43437dad6cedd5c559ff3
Jerome Laban <jerome.laban@nventive.com>
2023-01-30 14:03:05 -0500

    fix: Ensure build failure on emscripten install failure on Windows
    (cherry picked from commit bfeba140072443dfff8ecfc2b30d6e1141dcd21e)

--- src/Uno.Wasm.Bootstrap/build/scripts/emscripten-setup.cmd
+++ src/Uno.Wasm.Bootstrap/build/scripts/emscripten-setup.cmd
@@ -21,7 +21,15 @@ pushd "%UNO_EMSDK_PATH%"
 python --version
 
 IF ERRORLEVEL 1 ( 
-	echo Python is not installed. You can install it from the store https://www.microsoft.com/store/productId/9P7QFQMJRFP7  1>&2
+	echo Python is not installed. You can install it from the store https://aka.platform.uno/python-install 1>&2
+	EXIT /B 1
+)
+
+rem git --version
+git --version
+
+IF ERRORLEVEL 1 ( 
+	echo Git is not installed. You can install it from the store https://aka.platform.uno/bootstrapper-git 1>&2
 	EXIT /B 1
 )
 
@@ -29,9 +37,26 @@ IF NOT EXIST "%UNO_EMSDK_PATH_MARKER%" (
 	echo "Installing emscripten %UNO_EMSDK_VERSION% in %UNO_EMSDK_PATH%"
 
 	git clone --branch %UNO_EMSDK_VERSION% https://github.com/emscripten-core/emsdk 2>&1
+
+	IF ERRORLEVEL 1 ( 
+		echo Failed to clone the Emscripten SDK repository. Review the error logs for additional information and check https://aka.platform.uno/msbuild-troubleshoot 1>&2
+		EXIT /B 1
+	)
+
 	cd emsdk
 	emsdk install %UNO_EMSDK_VERSION% 2>&1
+
+	IF ERRORLEVEL 1 ( 
+		echo Failed to install Emscripten SDK %UNO_EMSDK_VERSION%. Review the error logs for additional information and check https://aka.platform.uno/msbuild-troubleshoot 1>&2
+		EXIT /B 1
+	)
+
 	emsdk activate %UNO_EMSDK_VERSION% 2>&1
+
+	IF ERRORLEVEL 1 ( 
+		echo Failed to activate the Emscripten SDK %UNO_EMSDK_VERSION%. Review the error logs for additional information and check https://aka.platform.uno/msbuild-troubleshoot 1>&2
+		EXIT /B 1
+	)
 
 	echo "Writing %UNO_EMSDK_PATH_MARKER%"
 	echo "installed" > "%UNO_EMSDK_PATH_MARKER%"
