fdd05cb7f43a1c47d9dab295b19fba835960f06c
Jerome Laban <jerome.laban@nventive.com>
2023-05-12 20:15:50 -0400

    chore: Adjust for net6 compatbililty

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -7,7 +7,7 @@ namespace Uno.Wasm.Bootstrap
 	internal class Constants
 	{
 		public const string DefaultDotnetRuntimeSdkUrl = "https://unowasmbootstrap.azureedge.net/runtime/"
-			+ "dotnet-runtime-wasm-linux-f961c1b-8adbca4855b-4950553939-Release.zip";
+			+ "dotnet-runtime-wasm-linux-dc15616-8adbca4855b-4963807262-Release.zip";
 
 		/// <summary>
 		/// Min version of the emscripten SDK. Must be aligned with dotnet/runtime SDK build in <see cref="NetCoreWasmSDKUri"/>.
