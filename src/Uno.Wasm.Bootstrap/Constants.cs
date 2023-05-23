b0c28db92beced1da5678fa1019c5a1eee35154f
Jerome Laban <jerome.laban@nventive.com>
2023-05-23 16:31:07 -0400

    chore: Adjust runtime for missing js string convert

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -7,7 +7,7 @@ namespace Uno.Wasm.Bootstrap
 	internal class Constants
 	{
 		public const string DefaultDotnetRuntimeSdkUrl = "https://unowasmbootstrap.azureedge.net/runtime/"
-			+ "dotnet-runtime-wasm-linux-79166df-e5c3811c762-5057343831-Release.zip";
+			+ "dotnet-runtime-wasm-linux-aecc4d7-e5c3811c762-5061371121-Release.zip";
 
 		/// <summary>
 		/// Min version of the emscripten SDK. Must be aligned with dotnet/runtime SDK build in <see cref="NetCoreWasmSDKUri"/>.
