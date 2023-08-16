fbb052421ab265226625a291c5dbc9d7b26c2cf9
Jerome Laban <jerome.laban@nventive.com>
2023-08-15 23:06:10 -0400

    feat: Update to .NET 8 Preview 7

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -7,7 +7,7 @@ namespace Uno.Wasm.Bootstrap
 	internal class Constants
 	{
 		public const string DefaultDotnetRuntimeSdkUrl = "https://unowasmbootstrap.azureedge.net/runtime/"
-			+ "dotnet-runtime-wasm-linux-7224aa4-57cfd062405-5385526085-Release.zip";
+			+ "dotnet-runtime-wasm-linux-5fcf14e-6d0e3e59dd2-5873392636-Release.zip";
 
 		/// <summary>
 		/// Min version of the emscripten SDK. Must be aligned with dotnet/runtime SDK build in <see cref="NetCoreWasmSDKUri"/>.
