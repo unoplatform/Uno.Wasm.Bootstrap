9fa934c39ca025bdea8af816c29330a004fec8c5
Jerome Laban <jerome.laban@nventive.com>
2023-09-01 08:50:10 -0400

    fix: Remove linked satellite assemblies refs in interp mode
    (cherry picked from commit 781b152318a229f2b3fb8bc5c9c863a62585d695)
    
    # Conflicts:
    #	src/Uno.Wasm.Bootstrap.sln
    #	src/Uno.Wasm.SampleNet7/Uno.Wasm.SampleNet7.csproj

---
+++ src/Uno.Wasm.Sample.Library/ClassLibrary01.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyMetadata("IsTrimmable", "True")]
+
+namespace Uno.Wasm.Sample.Library
+{
+	public class ClassLibrary01
+	{
+		private static void Unused()
+			=> typeof(Microsoft.CodeAnalysis.Accessibility).ToString();
+	}
+}
