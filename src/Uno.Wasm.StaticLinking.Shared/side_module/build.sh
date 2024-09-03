e64df7479e47e9c2bb9e63f4f07d956e4928a70c
Jerome Laban <jerome@platform.uno>
2024-09-03 00:01:42 -0400

    chore: Use .a files

--- src/Uno.Wasm.StaticLinking.Shared/side_module/build.sh
+++ src/Uno.Wasm.StaticLinking.Shared/side_module/build.sh
@@ -7,18 +7,18 @@ buildSideModule(){
 	emcc $1 -std=c++17 -s LEGALIZE_JS_FFI=0 -r -o $2 -s WASM=1 -fwasm-exceptions -msimd128 -s USE_LIBPNG=1 -DCUSTOM_VERSION="\"$3\"" $5 -DCUSTOM_FUNCTION_NAME="$4_getCustomVersion"
 }
 
-buildSideModule "mysideModule.cpp" "../native/side.bc/1.2/side.bc" "1.2" "side" ""
-buildSideModule "mysideModule.cpp" "../native/side.bc/1.2/st,simd/side.bc" "1.2" "side" "-msimd128"
+buildSideModule "mysideModule.cpp" "../native/side.a/1.2/side.a" "1.2" "side" ""
+buildSideModule "mysideModule.cpp" "../native/side.a/1.2/st,simd/side.a" "1.2" "side" "-msimd128"
 
-buildSideModule "version_test.cpp" "../native/side2.bc/1.3/side2.bc" "1.3" "side2" ""
-buildSideModule "version_test.cpp" "../native/side2.bc/1.2/st,simd/side2.bc" "1.2" "side2" "-msimd128"
+buildSideModule "version_test.cpp" "../native/side2.a/1.3/side2.a" "1.3" "side2" ""
+buildSideModule "version_test.cpp" "../native/side2.a/1.2/st,simd/side2.a" "1.2" "side2" "-msimd128"
 
-buildSideModule "version_test.cpp" "../native/side3.bc/1.3/side3.bc" "1.3" "side3" ""
-buildSideModule "version_test.cpp" "../native/side3.bc/1.4/side3.bc" "1.4" "side3" ""
-buildSideModule "version_test.cpp" "../native/side3.bc/1.4/st,simd/side3.bc" "1.4" "side3" "-msimd128"
+buildSideModule "version_test.cpp" "../native/side3.a/1.3/side3.a" "1.3" "side3" ""
+buildSideModule "version_test.cpp" "../native/side3.a/1.4/side3.a" "1.4" "side3" ""
+buildSideModule "version_test.cpp" "../native/side3.a/1.4/st,simd/side3.a" "1.4" "side3" "-msimd128"
 
-buildSideModule "version_test.cpp" "../native/side4.bc/1.3/side4.bc" "1.3" "side4" ""
-buildSideModule "version_test.cpp" "../native/side4.bc/2.0/side4.bc" "2.0" "side4" ""
-buildSideModule "version_test.cpp" "../native/side4.bc/3.1/side4.bc" "3.1" "side4" ""
-buildSideModule "version_test.cpp" "../native/side4.bc/3.1/st,simd/side4.bc" "3.1" "side4" "-msimd128"
-buildSideModule "version_test.cpp" "../native/side4.bc/5.0/side4.bc" "5.0" "side4" ""
+buildSideModule "version_test.cpp" "../native/side4.a/1.3/side4.a" "1.3" "side4" ""
+buildSideModule "version_test.cpp" "../native/side4.a/2.0/side4.a" "2.0" "side4" ""
+buildSideModule "version_test.cpp" "../native/side4.a/3.1/side4.a" "3.1" "side4" ""
+buildSideModule "version_test.cpp" "../native/side4.a/3.1/st,simd/side4.a" "3.1" "side4" "-msimd128"
+buildSideModule "version_test.cpp" "../native/side4.a/5.0/side4.a" "5.0" "side4" ""
