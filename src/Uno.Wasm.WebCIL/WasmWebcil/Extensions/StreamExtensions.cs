f3feeb9b27f48278fcb2fd2271899f5c3576f024
Jerome Laban <jerome@platform.uno>
2024-09-13 10:22:09 -0400

    chore: Update version checker to support WebCIL

---
+++ src/Uno.Wasm.WebCIL/WasmWebcil/Extensions/StreamExtensions.cs
@@ -0,0 +1,55 @@
+﻿//MIT License
+
+//Copyright(c) 2023 Stef Heyenrath
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+// Imported from https://github.com/StefH/ProtoBufJsonConverter/tree/dcacaf0ac07fe79c3f91c92746492b5c22b136cf/src-webcil/MetadataReferenceService.BlazorWasm
+
+using System.Runtime.InteropServices;
+
+namespace Uno.Wasm.WebCIL.Extensions;
+
+internal static class StreamExtensions
+{
+    internal static void WriteStruct<T>(this Stream stream, T structData) where T : struct
+    {
+        var bytes = StructToBytes(structData);
+        stream.Write(bytes);
+    }
+
+    private static byte[] StructToBytes<T>(T structData) where T : struct
+    {
+        int size = Marshal.SizeOf(structData);
+        byte[] byteArray = new byte[size];
+        nint ptr = Marshal.AllocHGlobal(size);
+
+        try
+        {
+            Marshal.StructureToPtr(structData, ptr, false);
+            Marshal.Copy(ptr, byteArray, 0, size);
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(ptr);
+        }
+
+        return byteArray;
+    }
+}
